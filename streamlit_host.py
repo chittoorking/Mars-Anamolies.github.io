8eb07bc4b64cb8f6ea4e2c27e704d8222c85536a
Chittoor Vamsi <56587705+chittoorking@users.noreply.github.com>
2022-01-12 20:56:11 +0530

    Update streamlit_host.py

--- streamlit_host.py
+++ streamlit_host.py
@@ -31,7 +31,7 @@ with header_container:
 
   
 
-  model = tf.keras.models.load_model("mdl_wts.hdf5")
+  model = tf.keras.models.load_model("https://github.com/chittoorking/Mars-Anamolies.github.io/blob/main/my_model.hdf5")
   ### load file
   uploaded_file = st.file_uploader("Choose a image file", type="jpg")
 
